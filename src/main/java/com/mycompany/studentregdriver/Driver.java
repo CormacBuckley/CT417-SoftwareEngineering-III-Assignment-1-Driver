29a526a3f87beb3790a57648357bd2c8f8654f67
Cormac Buckley <cormac.buckley@sap.com>
2018-10-20 19:44:49 +0100

    Setup of basic driver file

--- src/main/java/com/mycompany/studentregdriver/Driver.java
+++ src/main/java/com/mycompany/studentregdriver/Driver.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.studentregdriver;
+
+
+import com.mycompany.studentregistraionsystem.*;
+import org.joda.time.DateTime;
 
 /**
  *
@@ -11,7 +10,10 @@ package com.mycompany.studentregdriver;
  */
 public class Driver {
      public static void main(String[] args) {
-        
+   
+         CourseProgramme cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"), new DateTime("2013-08-16"));;
+         Student student = new Student("John Smith", 35, "01/01/2000", "1234", cp);
+         Module module = new Module("CT414", student, cp);
     }
     
 }
