7ee8d017e2a3a005b79f5b0284dc54a40d7c2c91
Cormac Buckley <cormac.buckley@sap.com>
2018-10-20 20:25:06 +0100

    Added to Driver file

--- src/main/java/com/mycompany/studentregdriver/Driver.java
+++ src/main/java/com/mycompany/studentregdriver/Driver.java
@@ -1,7 +1,7 @@
 package com.mycompany.studentregdriver;
 
-
 import com.mycompany.studentregistraionsystem.*;
+import java.util.ArrayList;
 import org.joda.time.DateTime;
 
 /**
@@ -9,11 +9,37 @@ import org.joda.time.DateTime;
  * @author I342042
  */
 public class Driver {
-     public static void main(String[] args) {
-   
-         CourseProgramme cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"), new DateTime("2013-08-16"));;
-         Student student = new Student("John Smith", 35, "01/01/2000", "1234", cp);
-         Module module = new Module("CT414", student, cp);
+
+    public static void main(String[] args) {
+
+        ArrayList<Module> moduleList = new ArrayList<Module>();
+
+        Module module = null;
+        Student student;
+        CourseProgramme cp;
+
+        Student[] stud = new Student[10];
+
+        cp = new CourseProgramme("BCT", module, new DateTime("2012-08-16"), new DateTime("2013-08-16"));;
+        student = new Student("John Smith", 35, "01/01/2000", "1234", cp);
+        module = new Module("CT414", student, cp);
+
+        Module CompScience = new Module("CT414", student, cp);
+
+        student.setName("John Smith");
+
+        stud[0] = student;
+        
+        moduleList.add(CompScience);
+
+       
+            System.out.println("Modules:");
+            System.out.println(moduleList.get(0).getName());
+            System.out.println("Students registered for module:");
+            System.out.println(moduleList.get(0).getStudents()[0].getName());
+
+        
+
     }
-    
+
 }
